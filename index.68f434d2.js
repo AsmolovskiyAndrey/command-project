13130e501835f7879fe0127eb6245e401b82a0a1
AsmolovskiyAndrey <105419171+AsmolovskiyAndrey@users.noreply.github.com>
2022-08-09 17:48:32 +0000

    Deploying to gh-pages from @ AsmolovskiyAndrey/command-project@1bee55fe58dd3593da66fe26c28e1556720e1fa1 🚀

--- index.68f434d2.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=index.68f434d2.js.map
--- index.8a002fb7.js
+++
@@ -1,2 +0,0 @@
-$((function(){$(".rev_slider").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0})}));
-//# sourceMappingURL=index.8a002fb7.js.map
